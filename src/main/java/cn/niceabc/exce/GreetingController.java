c9dd524f612541bea84862952ad1bd2ba006cd3f
shiguang.ma <shiguang.ma@nd-english.com>
2019-04-01 09:52:25 +0800

    send err

--- src/main/java/cn/niceabc/exce/GreetingController.java
+++ src/main/java/cn/niceabc/exce/GreetingController.java
@@ -1,10 +1,27 @@
 package cn.niceabc.exce;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class GreetingController {
+
+    @RequestMapping("/err")
+    public Greeting err(HttpServletResponse response) throws IOException {
+        Greeting g = new Greeting();
+        g.setId(1L);
+        g.setContent("hello.");
+
+        if (1 == 1) {
+            response.sendError(HttpStatus.BAD_REQUEST.value(), "wtf. bad request");
+        }
+
+        return g;
+    }
 
     @RequestMapping("/greeting")
     public Greeting greeting() {
